c36823c0a80df9b88f317b2172542abcc3af8103
jy3026 <jy_3026@naver.com>
2023-06-17 23:36:35 +0900

    초기 세팅

---
+++ server/src/main/java/choikang/MealGuard/MealGuardApplication.java
@@ -0,0 +1,13 @@
+package choikang.MealGuard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealGuardApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/choikang/MealGuard/MealGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package choikang.MealGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
