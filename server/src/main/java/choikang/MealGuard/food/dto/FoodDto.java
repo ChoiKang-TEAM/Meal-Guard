718b4784064aacc2571929c116b3227a81906138
jy3026 <jy_3026@naver.com>
2023-06-18 03:12:17 +0900

    feat: 시간대별 맞춤 음식 추천 구현

--- server/src/main/java/choikang/MealGuard/food/dto/FoodDto.java
+++ server/src/main/java/choikang/MealGuard/food/dto/FoodDto.java
@@ -1,10 +1,12 @@
 package choikang.MealGuard.food.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 public class FoodDto {
 
     @Getter
+    @Setter
     public static class Response{
 
         private long id;
--- server/src/main/java/choikang/MealGuard/food/repository/FoodRepository.java
+++ server/src/main/java/choikang/MealGuard/food/repository/FoodRepository.java
@@ -2,6 +2,13 @@ package choikang.MealGuard.food.repository;
 
 import choikang.MealGuard.food.entity.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FoodRepository extends JpaRepository<Food,Long> {
+
+    @Query("select f from Food f join f.times t where t.hours = :hours")
+    List<Food> findAllByHours(@Param("hours") String hours);
 }
