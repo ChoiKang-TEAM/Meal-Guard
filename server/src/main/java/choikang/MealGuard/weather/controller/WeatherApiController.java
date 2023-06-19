a24e814a2153144e7c26433cdcaaf4737884188e
jy3026 <jy_3026@naver.com>
2023-06-19 15:08:35 +0900

    fix: csv 파일 읽어오는 컨트롤러 계층 나누기

--- server/src/main/java/choikang/MealGuard/weather/controller/WeatherApiController.java
+++ server/src/main/java/choikang/MealGuard/weather/controller/WeatherApiController.java
@@ -1,14 +1,9 @@
 package choikang.MealGuard.weather.controller;
 
 import choikang.MealGuard.weather.dto.WeatherDto;
-import choikang.MealGuard.weather.entity.Region;
-import choikang.MealGuard.weather.entity.Weather;
 import choikang.MealGuard.weather.service.WeatherApiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,16 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.EntityManager;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/api/weather")
