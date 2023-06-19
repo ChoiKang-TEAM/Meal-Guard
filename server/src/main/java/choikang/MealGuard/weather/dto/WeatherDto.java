0d082313065cce41b4a106acacdfc186ea2e639c
jy3026 <jy_3026@naver.com>
2023-06-19 13:42:34 +0900

    feat: 날씨 응답 Dto 설계

---
+++ server/src/main/java/choikang/MealGuard/weather/dto/WeatherDto.java
@@ -0,0 +1,19 @@
+package choikang.MealGuard.weather.dto;
+
+import choikang.MealGuard.weather.entity.Weather;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+public class WeatherDto {
+
+    @Getter
+    @Setter
+    @Builder
+    public static class Response{
+
+        private Weather weather;
+
+        private String message;
+    }
+}
