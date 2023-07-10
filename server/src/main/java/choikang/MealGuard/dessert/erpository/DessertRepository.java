23bb75c934ad9fee5418eacd7b8a3d64c27fff9b
jy3026 <jy_3026@naver.com>
2023-07-10 20:28:59 +0900

    feat: 디저트 레포지토리 구현

---
+++ server/src/main/java/choikang/MealGuard/dessert/erpository/DessertRepository.java
@@ -0,0 +1,7 @@
+package choikang.MealGuard.dessert.erpository;
+
+import choikang.MealGuard.dessert.entity.Dessert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DessertRepository extends JpaRepository<Dessert,Long> {
+}
