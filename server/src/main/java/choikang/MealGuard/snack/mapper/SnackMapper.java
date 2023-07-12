4416f35583887ff5851ad00984941c2944fbb2c5
jy3026 <jy_3026@naver.com>
2023-07-12 17:03:31 +0900

    fix: 안주 mapper 구현

--- server/src/main/java/choikang/MealGuard/snack/mapper/SnackMapper.java
+++ server/src/main/java/choikang/MealGuard/snack/mapper/SnackMapper.java
@@ -1,4 +1,12 @@
 package choikang.MealGuard.snack.mapper;
 
+import choikang.MealGuard.snack.dto.SnackDto;
+import choikang.MealGuard.snack.entity.Snack;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SnackMapper {
+
+    SnackDto.Response snackToResponse(Snack snack);
 }
