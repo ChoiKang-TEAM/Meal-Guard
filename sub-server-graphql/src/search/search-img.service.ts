72f3f80b154f02b44cc2407c6d2841c269a939d5
motojate <motojate@naver.com>
2023-07-14 16:19:06 +0900

    * axios 구현

--- sub-server-graphql/src/search/search-img.service.ts
+++ sub-server-graphql/src/search/search-img.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@nestjs/common'
+import axios from 'axios'
 
 @Injectable()
 export class SearchImgService {
   async sampleQuery(keyword: string): Promise<string> {
     const Access_Key = process.env.UNSPLASH_ACCESS_KEY
-    const url = `https://openapi.naver.com/v1/search/image.json?query=${keyword}&display=1`
+    const url = `https://openapi.naver.com/v1/search/image`
     try {
-      const data = await fetch(url, {
+      const { data } = await axios.get(url, {
         headers: {
+          'Content-Type': 'application/json',
           'X-Naver-Client-Id': 'TojrHxgNpXmUA8f78mOU',
           'X-Naver-Client-Secret': 'CDe8KaDUJw'
+        },
+        params: {
+          query: keyword,
+          filter: 'small',
+          display: 1
         }
       })
       // const data = await (await fetch(url, {
       //   headers:}
 
       // })).json()
-      const result = await data.json()
+      console.log(data?.items?.[0].thumbnail)
 
-      return result?.items?.[0]?.link
+      return data?.items?.[0].thumbnail
     } catch (e) {
       console.error(e)
     }
