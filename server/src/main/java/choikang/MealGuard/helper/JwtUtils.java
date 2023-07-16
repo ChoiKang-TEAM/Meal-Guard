9815082649b0628118d4f2348e460ab39ab80426
jy3026 <jy_3026@naver.com>
2023-07-16 14:27:08 +0900

    feat:  jwt토큰을 파싱해 claims를 얻어오는 로직 구현

---
+++ server/src/main/java/choikang/MealGuard/helper/JwtUtils.java
@@ -0,0 +1,31 @@
+package choikang.MealGuard.helper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtils {
+    @Value("${jwt.secret-key}")
+    private String SECRET_KEY;
+
+    public Claims parseToken(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
