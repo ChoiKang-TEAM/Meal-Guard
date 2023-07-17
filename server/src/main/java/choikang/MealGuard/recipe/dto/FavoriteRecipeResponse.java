2778bb936fc40f32c1d287e7db21445e4ec7f2aa
jy3026 <jy_3026@naver.com>
2023-07-17 19:53:35 +0900

    feat: 좋아요한 레시피 응답데이터 구현

---
+++ server/src/main/java/choikang/MealGuard/recipe/dto/FavoriteRecipeResponse.java
@@ -0,0 +1,15 @@
+package choikang.MealGuard.recipe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class FavoriteRecipeResponse {
+    private Long recipeId;
+    private String mainImage;
+    private String name;
+    private String kcal;
+}
