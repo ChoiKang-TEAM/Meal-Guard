1dbe08644aa6d4208f21cc4d90cacbe3e8f3734d
jy3026 <jy_3026@naver.com>
2023-07-17 19:54:36 +0900

    feat: 좋아요 레포지토리 구현

---
+++ server/src/main/java/choikang/MealGuard/recipe/repository/FavoriteRepository.java
@@ -0,0 +1,17 @@
+package choikang.MealGuard.recipe.repository;
+
+import choikang.MealGuard.recipe.dto.FavoriteRecipeResponse;
+import choikang.MealGuard.recipe.dto.RecipeDto;
+import choikang.MealGuard.recipe.entity.Favorite;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface FavoriteRepository extends JpaRepository<Favorite,Long> {
+
+    @Query("SELECT new choikang.MealGuard.recipe.dto.FavoriteRecipeResponse(r.recipeId, r.mainImage, r.name, r.kcal) " +
+            "FROM Favorite f JOIN Recipe r ON f.recipe.recipeId = r.recipeId " +
+            "WHERE f.user.user_seq = :userSeq")
+    Page<FavoriteRecipeResponse> findFavoriteRecipesByUser_seq(String userSeq, Pageable pageable);
+}
