13dcc85e539f6d21bbc4b178cd4f94f416933b0b
motojate <motojate@naver.com>
2023-07-18 18:09:39 +0900

    * 메인 레이아웃 수정 중

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -148,106 +148,104 @@ export default defineComponent({
 </script>
 
 <template>
-  <q-page-container>
-    <q-header style="margin-top: 50px">
-      <q-tabs class="bg-black" v-model="activeTabIndex">
-        <q-tab
-          v-for="(tab, index) in HOME_TABS"
-          :key="index"
-          @click="scrollToSection(index)"
-          :alert="tab.alert"
-          :icon="tab.icon"
-          :name="tab.name"
-        />
-      </q-tabs>
-    </q-header>
+  <q-header style="margin-top: 50px">
+    <q-tabs class="bg-black" v-model="activeTabIndex">
+      <q-tab
+        v-for="(tab, index) in HOME_TABS"
+        :key="index"
+        @click="scrollToSection(index)"
+        :alert="tab.alert"
+        :icon="tab.icon"
+        :name="tab.name"
+      />
+    </q-tabs>
+  </q-header>
 
-    <q-card class="bg-yellow-3 section">
-      <q-card-section>
-        <div class="weather-widget">
-          <div class="location">{{ rotateData?.region }}</div>
-          <div class="temperature">{{ rotateData?.weather?.temp }}°C</div>
-          <div class="description">
-            <ion-icon v-if="rotateData?.weather?.sky" name="snow" />
-            {{ rotateData?.weather?.sky }}
-          </div>
+  <q-card class="bg-yellow-3 section">
+    <q-card-section>
+      <div class="weather-widget">
+        <div class="location">{{ rotateData?.region }}</div>
+        <div class="temperature">{{ rotateData?.weather?.temp }}°C</div>
+        <div class="description">
+          <!-- <ion-icon v-if="rotateData?.weather?.sky" name="snow" /> -->
+          {{ rotateData?.weather?.sky }}
         </div>
-        <div class="parallax" ref="parallax">
-          <div>
-            <div class="lb-text">
-              <h2 class="title-text">식사 지킴이</h2>
-            </div>
-            <q-parallax class="meal-image" :src="randomImgUrl" />
-          </div>
-        </div>
-      </q-card-section>
-    </q-card>
-    <q-card>
-      <q-card-section horizontal>
-        <q-img
-          class="col-5"
-          src="https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_1280.jpg"
-          style="height: 500px"
-        />
-      </q-card-section>
-
-      <q-separator />
-    </q-card>
-
-    <q-card class="bg-pink-2 section">
-      <div class="flex justify-center items-center">
-        <q-parallax
-          src="src/assets/images/desserts.png"
-          :height="500"
-          style="width: 800px"
-        />
       </div>
-    </q-card>
-    <q-card flat>
-      <q-card-section horizontal>
-        <q-img
-          class="col-5"
-          src="https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_1280.jpg"
-        />
-
-        <q-card-section class="text-black"> dd </q-card-section>
-      </q-card-section>
-
-      <q-separator />
-    </q-card>
-    <q-card class="my-card section" flat bordered>
-      <q-card-section horizontal>
-        <q-card-section class="q-pt-xs">
-          <div class="text-overline text-black">Meal Guard</div>
-          <div class="text-h5 q-mt-sm q-mb-xs text-black">추천 레시피</div>
-          <div class="text-caption text-grey">
-            오늘의 한 끼, 건강한 한 끼, 모두의 한 끼
+      <div class="parallax" ref="parallax">
+        <div>
+          <div class="lb-text">
+            <h2 class="title-text">식사 지킴이</h2>
           </div>
-        </q-card-section>
+          <q-parallax class="meal-image" :src="randomImgUrl" />
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+  <q-card>
+    <q-card-section horizontal>
+      <q-img
+        class="col-5"
+        src="https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_1280.jpg"
+        style="height: 500px"
+      />
+    </q-card-section>
 
-        <q-card-section class="col-5 flex flex-center">
-          <q-img
-            class="rounded-borders"
-            src="https://cdn.quasar.dev/img/parallax2.jpg"
-          />
-        </q-card-section>
+    <q-separator />
+  </q-card>
+
+  <q-card class="bg-pink-2 section">
+    <div class="flex justify-center items-center">
+      <q-parallax
+        src="src/assets/images/desserts.png"
+        :height="500"
+        style="width: 800px"
+      />
+    </div>
+  </q-card>
+  <q-card flat>
+    <q-card-section horizontal>
+      <q-img
+        class="col-5"
+        src="https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_1280.jpg"
+      />
+
+      <q-card-section class="text-black"> dd </q-card-section>
+    </q-card-section>
+
+    <q-separator />
+  </q-card>
+  <q-card class="my-card section" flat bordered>
+    <q-card-section horizontal>
+      <q-card-section class="q-pt-xs">
+        <div class="text-overline text-black">Meal Guard</div>
+        <div class="text-h5 q-mt-sm q-mb-xs text-black">추천 레시피</div>
+        <div class="text-caption text-grey">
+          오늘의 한 끼, 건강한 한 끼, 모두의 한 끼
+        </div>
       </q-card-section>
 
-      <q-separator />
-
-      <q-card-actions>
-        <q-btn
-          @click="openViewModal(2)"
-          flat
-          color="primary"
-          label="레시피 보기"
+      <q-card-section class="col-5 flex flex-center">
+        <q-img
+          class="rounded-borders"
+          src="https://cdn.quasar.dev/img/parallax2.jpg"
         />
-      </q-card-actions>
-      <q-dialog v-model="viewModalState[2].state"
-        ><recipe-table-dialog style="max-width: 1080px"></recipe-table-dialog
-      ></q-dialog>
-    </q-card>
-  </q-page-container>
+      </q-card-section>
+    </q-card-section>
+
+    <q-separator />
+
+    <q-card-actions>
+      <q-btn
+        @click="openViewModal(2)"
+        flat
+        color="primary"
+        label="레시피 보기"
+      />
+    </q-card-actions>
+    <q-dialog v-model="viewModalState[2].state"
+      ><recipe-table-dialog style="max-width: 1080px"></recipe-table-dialog
+    ></q-dialog>
+  </q-card>
 </template>
 
 <style lang="scss" scoped>
