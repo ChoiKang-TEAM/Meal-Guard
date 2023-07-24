64f0c5b1b31d7d4146c0b0ad34e644e1a604133f
jy3026 <jy_3026@naver.com>
2023-07-24 19:13:03 +0900

    feat: 최근 본 레시피를 조회하는 엔드포인트 구현

--- server/src/main/java/choikang/MealGuard/user/controller/UserController.java
+++ server/src/main/java/choikang/MealGuard/user/controller/UserController.java
@@ -1,10 +1,16 @@
 package choikang.MealGuard.user.controller;
 
+import choikang.MealGuard.user.dto.RecentRecipeResponse;
+import choikang.MealGuard.user.entity.RecentRecipe;
+import choikang.MealGuard.user.mapper.RecentRecipeMapper;
 import choikang.MealGuard.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -13,10 +19,22 @@ public class UserController {
 
     private final UserService userService;
 
+    private final RecentRecipeMapper recentRecipeMapper;
     @PostMapping("/create")
     public ResponseEntity<String> getUserId(@RequestBody String user_seq){
         userService.createUser(user_seq);
 
         return ResponseEntity.ok("유저 저장 완료");
     }
+
+    @GetMapping("/recent-recipes")
+    public ResponseEntity getRecentRecipes(@RequestHeader("Authorization") String token){
+        List<RecentRecipe> recentRecipes = userService.getRecentRecipes(token);
+
+        List<RecentRecipeResponse> recentRecipeResponses = recentRecipeMapper.recentRecipesToResponses(recentRecipes);
+        Collections.reverse(recentRecipeResponses);
+
+        return new ResponseEntity(recentRecipeResponses, HttpStatus.OK);
+    }
+
 }
