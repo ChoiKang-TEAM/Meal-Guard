7eb227193ec4ffc6094b5f0cb6678c979cb8fbbb
motojate <motojate@naver.com>
2023-07-24 19:11:55 +0900

    * 컴포넌트화 시키기

---
+++ client/src/components/recipe/RecipeBlogCard.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const pageIndex = ref<number>(1)
+
+    const state = {}
+    const action = {}
+    return {
+      ...state,
+      ...action,
+    }
+  },
+})
+</script>
+
+<template>
+  <q-card>dd </q-card>
+</template>
