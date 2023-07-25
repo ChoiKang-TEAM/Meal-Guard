728cd71aa397117cc670c0e26e5093814e0f5c9a
motojate <motojate@naver.com>
2023-07-25 23:48:56 +0900

    * 퍼블리싱 연결

--- client/src/components/recipe/RecipeBlogCard.vue
+++ client/src/components/recipe/RecipeBlogCard.vue
@@ -1,12 +1,35 @@
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
+import { RecipeBlogCardModel } from 'src/common/models'
 
 export default defineComponent({
-  setup() {
-    const pageIndex = ref<number>(1)
+  props: {
+    blogData: {
+      type: Object as () => RecipeBlogCardModel,
+      required: true,
+    },
+  },
+  setup(props) {
+    const blodCardData = computed(() => props.blogData)
+    const isHovered = ref(false)
+    const showButtons = () => {
+      isHovered.value = true
+    }
 
-    const state = {}
-    const action = {}
+    const hideButtons = () => {
+      isHovered.value = false
+    }
+
+    const handleButton1Click = () => {
+      // Logic for Button 1 click
+    }
+
+    const handleButton2Click = () => {
+      // Logic for Button 2 click
+    }
+    console.log(props.blogData)
+    const state = { isHovered, blodCardData }
+    const action = { showButtons, hideButtons }
     return {
       ...state,
       ...action,
@@ -16,5 +39,54 @@ export default defineComponent({
 </script>
 
 <template>
-  <q-card>dd </q-card>
+  <q-card
+    class="recipe-blog-card text-black"
+    @mouseenter="showButtons"
+    @mouseleave="hideButtons"
+    :style="{
+      backgroundColor: isHovered
+        ? 'rgba(128, 128, 128, 0.8)'
+        : 'rgba(255, 255, 255, 1)',
+    }"
+  >
+    <q-img :src="blodCardData.thumbnail" spinner-color="white">
+      <div
+        v-if="isHovered"
+        class="absolute-full text-subtitle2 flex flex-center"
+      />
+    </q-img>
+
+    <q-card-section>
+      <div class="text-h6" style="height: 100px">
+        <p v-html="blodCardData.title"></p>
+      </div>
+    </q-card-section>
+    <q-separator inset />
+    <q-card-section><p v-html="blodCardData.contents"></p></q-card-section>
+    <q-card-actions align="center" v-if="isHovered"
+      ><q-btn outline label="최진영" icon="star" />
+      <q-btn outline label="안찬양"
+    /></q-card-actions>
+  </q-card>
 </template>
+
+<style>
+.wrapper {
+  position: relative;
+  /* Add other styles as needed */
+}
+
+.buttons {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  /* Add other styles for the buttons container */
+}
+
+.buttons button {
+  margin: 5px;
+  /* Add other styles for the buttons as needed */
+}
+</style>
