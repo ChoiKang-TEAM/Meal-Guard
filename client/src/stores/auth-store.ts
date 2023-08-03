408ee82e78db97fba4ecdd09db3b77e86404fe75
motojate <motojate@naver.com>
2023-08-03 23:46:59 +0900

    * 회원가입 시 인풋 데이터 저장

--- client/src/stores/auth-store.ts
+++ client/src/stores/auth-store.ts
@@ -3,9 +3,15 @@ import { useMutation } from 'villus'
 import { LOGIN_MEMBER_USER, SIGN_UP_MEMBER_USER } from 'src/graphql/member-user'
 
 export const useAuthStore = defineStore('auth', () => {
+  const signUpInputData = new Map()
+  signUpInputData.set('privacy-policy', {
+    one: true,
+    two: true,
+  })
+
   const { execute: loginMemberUser } = useMutation(LOGIN_MEMBER_USER)
   const { execute: signUpMemberUser } = useMutation(SIGN_UP_MEMBER_USER)
-  const state = {}
+  const state = { signUpInputData }
   const action = {
     loginMemberUser,
     signUpMemberUser,
