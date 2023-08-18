e30c9fcbe490466ee59ef3f228dc7cdd31335bd5
motojate <motojate@naver.com>
2023-08-19 01:57:40 +0900

    * 선호 음식 조사 로직 구성 완료

--- client/src/components/sign-up/PreferredFoodCheck.vue
+++ client/src/components/sign-up/PreferredFoodCheck.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { storeToRefs } from 'pinia'
 import { CategoryFindModel } from 'src/common/models'
+import { noticeDialog } from 'src/common/utils'
 import { FindFilterFoodInput } from 'src/graphql/dto/food-input'
 import { useAuthStore } from 'src/stores/auth-store'
 import { useCategoryStore } from 'src/stores/category-store'
@@ -9,46 +10,28 @@ import { defineComponent, onMounted, ref } from 'vue'
 
 export default defineComponent({
   setup() {
-    const authStore = useAuthStore()
     const categoryStore = useCategoryStore()
-    const foodStore = useFoodStore()
     const { findAllCategory } = categoryStore
-    const { findFoodByFilter } = foodStore
-    const { categoryTabData } = storeToRefs(categoryStore)
-    const tab = ref<string>('')
-    const foodData = ref<{ id: number; name: string }[]>([])
+    const { categoryData } = storeToRefs(categoryStore)
     const preferredFoodList = ref<number[]>([])
 
     onMounted(async () => {
       await findAllCategory()
-      tab.value = categoryTabData.value[0].type
-      await changeFoodData()
     })
 
     const goNextStep = () => {
       console.log(1)
     }
 
-    const changeFoodData = async (): Promise<void> => {
-      const categoryId: number = findCategoryIdByCategoryName()
-      foodData.value = await findFoodByFilter({ categoryId })
+    const checkByValidCheckBox = (event: number[]) => {
+      if (event.length > 3) {
+        event.pop()
+        return noticeDialog('선호 음식은 3가지만 선택이 가능합니다.')
+      }
     }
 
-    const findCategoryIdByCategoryName = (): number => {
-      const foundData: CategoryFindModel | undefined =
-        categoryTabData.value.find(
-          (val: CategoryFindModel) => val.type === tab.value
-        )
-
-      return foundData?.id ?? 0
-    }
-
-    const test = (event: any) => {
-      console.log(preferredFoodList.value)
-    }
-
-    const state = { categoryTabData, tab, foodData, preferredFoodList }
-    const action = { goNextStep, changeFoodData, test }
+    const state = { categoryData, preferredFoodList }
+    const action = { goNextStep, checkByValidCheckBox }
     return {
       ...state,
       ...action,
@@ -65,50 +48,25 @@ export default defineComponent({
     </div></q-card-section
   >
   <q-card-section class="text-black">
-    <q-tabs
-      v-model="tab"
-      dense
-      class="text-grey"
-      active-color="positive"
-      indicator-color="positive"
-      align="justify"
-      narrow-indicator
-      @update:model-value="changeFoodData"
+    <q-item-label header
+      >선호 음식 종류
+      <div class="caption-div">최대 3종류 선택</div></q-item-label
     >
-      <q-tab
-        v-for="(data, index) in categoryTabData"
-        :key="index"
-        :name="data.type"
-        :label="data.type"
-        style="border-radius: 40px"
-      />
-    </q-tabs>
 
-    <q-separator />
-
-    <q-tab-panels v-model="tab" animated>
-      <q-tab-panel
-        v-for="(data, index) in categoryTabData"
-        :key="index"
-        :name="data.type"
-      >
-        <q-item-label header>{{ data.type }}</q-item-label>
-
-        <q-item v-for="item in foodData" :key="item.id" tag="label" v-ripple>
-          <q-item-section side top>
-            <q-checkbox
-              v-model="preferredFoodList"
-              @click="test"
-              :val="item.id"
-            />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>{{ item.name }}</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-tab-panel>
-    </q-tab-panels></q-card-section
-  >
+    <q-item v-for="item in categoryData" :key="item.id" tag="label" v-ripple>
+      <q-item-section side top>
+        <q-checkbox
+          v-model="preferredFoodList"
+          :val="item.id"
+          color="positive"
+          @update:model-value="checkByValidCheckBox"
+        />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ item.type }}</q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-card-section>
   <q-card-actions>
     <q-btn flat label="건너뛰기" class="bg-grey" @click="goNextStep" />
     <q-space />
--- client/src/stores/category-store.ts
+++ client/src/stores/category-store.ts
@@ -5,19 +5,19 @@ import { useQuery } from 'villus'
 import { ref } from 'vue'
 
 export const useCategoryStore = defineStore('category', () => {
-  const categoryTabData = ref<CategoryFindModel[]>([])
+  const categoryData = ref<CategoryFindModel[]>([])
   const findAllCategory = async () => {
     try {
       const { data } = await useQuery({
         query: FIND_ALL_CATEGORY,
         cachePolicy: 'network-only',
       })
-      categoryTabData.value = data?.value?.findAllCategory ?? []
+      categoryData.value = data?.value?.findAllCategory ?? []
     } catch (e) {
       console.error(e)
     }
   }
-  const state = { categoryTabData }
+  const state = { categoryData }
   const action = { findAllCategory }
 
   return { ...state, ...action }
